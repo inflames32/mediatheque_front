bc49ef53f2dd0db993e1488febeae7cda1bc8445
MickGe <mickge.git@netc.fr>
2021-07-21 21:39:50 +0200

    (merge) merge from developpement branch

--- src/Components/AlbumDetails/index.js
+++ src/Components/AlbumDetails/index.js
@@ -7,33 +7,33 @@ import { BsTrash, BsPencil } from "react-icons/bs";
 import "../../Styles/album_details.scss";
 
 const AlbumDetails = ({
-  setAlbumDetailsIsOpen,
-  setIncrement,
-  increment,
-  albumId,
+	setAlbumDetailsIsOpen,
+	setIncrement,
+	increment,
+	albumId,
 }) => {
-  const [key, setKey] = useState(0);
-  const { deleteAlbumByID, getOneAlbum, album } = AlbumsMiddleware();
+	const [key, setKey] = useState(0);
+	const { deleteAlbumByID, getOneAlbum, album } = AlbumsMiddleware();
 
 	useEffect(() => {
 		getOneAlbum(albumId);
 	}, [key]); // eslint-disable-line react-hooks/exhaustive-deps
 
-  const deleteAlbum = (albumId) => () => {
-    deleteAlbumByID(albumId);
-    setIncrement(key + 1);
-  };
+	const deleteAlbum = (albumId) => () => {
+		deleteAlbumByID(albumId);
+		setIncrement(key + 1);
+	};
 
-  const closeAlbumDetails = () => {
-    setAlbumDetailsIsOpen(false);
-  };
+	const closeAlbumDetails = () => {
+		setAlbumDetailsIsOpen(false);
+	};
 
-  const handleUpdateAlbumDetails = () => {
-    console.log("je modifierais les infos de l'album");
-  };
+	const handleUpdateAlbumDetails = () => {
+		console.log("je modifierais les infos de l'album");
+	};
 
-  const ImgNotDefined =
-    "https://image.flaticon.com/icons/png/128/376/376819.png";
+	const ImgNotDefined =
+		"https://image.flaticon.com/icons/png/128/376/376819.png";
 
 	return (
 		<div className='modal-albumDetails' onClick={closeAlbumDetails}>
@@ -44,46 +44,50 @@ const AlbumDetails = ({
 				<Card.Body>
 					<span className='btn-close' onClick={closeAlbumDetails}></span>
 
-          <Card.Text>
-            <span>
-              {album.cover ? (
-                <div className="modal-content-albumDetails-cover">
-                  <Card.Img src={album.cover} />
-                </div>
-              ) : (
-                <div className="modal-content-albumDetails-cover">
-                  <Card.Img src={ImgNotDefined} />
-                </div>
-              )}
-              {/* <div>album id : {album._id}</div> */}
-              <div>Nom de l'album : {album.name}</div>
-              <div>Nom de l'artiste : {album.artist}</div>
-              <div>Style : {album.style}</div>
-              <div>Année : {album.year}</div>
-            </span>
-            <span>
-              <BsPencil
-                className="pencil-icon"
-                onClick={handleUpdateAlbumDetails}
-              />
-            </span>
-            <br /> <br />
-            <br />
-          </Card.Text>
-          <Button
-            onClick={closeAlbumDetails}
-            className="albumDetails-btn-close"
-            variant="primary"
-          >
-            OK
-          </Button>
-          <span>
-            <BsTrash className="trash-icon" onClick={deleteAlbum(albumId)} />
-          </span>
-        </Card.Body>
-      </Card>
-    </div>
-  );
+					<Card.Text>
+						<span>
+							{album.cover ? (
+								<span className='modal-content-albumDetails-cover'>
+									<Card.Img src={album.cover} />
+								</span>
+							) : (
+								<span className='modal-content-albumDetails-cover'>
+									<Card.Img src={ImgNotDefined} />
+								</span>
+							)}
+							{/* <div>album id : {album._id}</div> */}
+							{/* TODO: ajouter une class pour les display block */}
+							<span style={{ display: "block" }}>
+								Nom de l'album : {album.name}
+							</span>
+							<span style={{ display: "block" }}>
+								Nom de l'artiste : {album.artist}
+							</span>
+							<span style={{ display: "block" }}>Style : {album.style}</span>
+							<span style={{ display: "block" }}>Année : {album.year}</span>
+						</span>
+						<span style={{ display: "block" }}>
+							<BsPencil
+								className='pencil-icon'
+								onClick={handleUpdateAlbumDetails}
+							/>
+						</span>
+						<br /> <br />
+						<br />
+					</Card.Text>
+					<Button
+						onClick={closeAlbumDetails}
+						className='albumDetails-btn-close'
+						variant='primary'>
+						OK
+					</Button>
+					<span>
+						<BsTrash className='trash-icon' onClick={deleteAlbum(albumId)} />
+					</span>
+				</Card.Body>
+			</Card>
+		</div>
+	);
 };
 
 export default AlbumDetails;
